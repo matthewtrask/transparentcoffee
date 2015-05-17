b1aaff64f8f7902bfa93dd6d6594372a31516d44
matt trask <mjftrask@gmail.com>
2015-05-17 18:15:14 -0400

    controller functions added

---
+++ public/app/controllers/ttc.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace controllers;
+
+use helpers\url;
+use helpers\form;
+use helpers\phpmailer\mail;
+use core\view;
+
+class ttc extends \core\controller{
+
+	public function index() {
+		$data['title'] = 'Home';
+
+		View::rendertemplate('header', $data);
+		View::rendertemplate('footer');
+	}
+
+	public function about() {
+		$data['title'] = 'About';
+
+	}
+
+	public function transparency() {
+		$data['title'] = 'Transparency';
+	}
+
+	public function SEG() {
+		$data['title'] = 'SEG';
+
+	}
+
+	public function ttcoffees() {
+		$data['title'] = 'Transparent Coffees';
+	}
+
+	public function register() {
+		$data['title'] = 'Register';
+	}
+
+	public function insight() {
+		$data['title'] = 'Insights';
+	}
+
+	public function scrpi() {
+		$data['title'] = 'SCRPI';
+	}
+
+	public function roasters() {
+		$data['title'] = 'Roasters';
+	}
+
+	public function contact() {
+		$data['title'] = 'Contact'
+	}
+}
