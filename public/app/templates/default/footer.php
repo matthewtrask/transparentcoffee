96f9b8ef3e228d7025c83e855c06d2368303faf8
matt trask <mjftrask@gmail.com>
2015-05-17 17:51:52 -0400

    added basic footer tags

--- public/app/templates/default/footer.php
+++ public/app/templates/default/footer.php
@@ -1,13 +1,8 @@
 
 </div>
 
-<!-- JS -->
-<?php
-	helpers\assets::js(array(
-		helpers\url::template_path() . 'js/jquery.js',
-		'//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'
-	)) 
-?>
+<footer>
+</footer>
 
 </body>
 </html>
