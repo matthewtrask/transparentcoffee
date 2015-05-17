c0b6628018494f21fb090489c73692b296bc9e06
matt trask <mjftrask@gmail.com>
2015-05-17 11:44:23 -0400

    added simplemvc for our framework

---
+++ public/app/templates/default/footer.php
@@ -0,0 +1,13 @@
+
+</div>
+
+<!-- JS -->
+<?php
+	helpers\assets::js(array(
+		helpers\url::template_path() . 'js/jquery.js',
+		'//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'
+	)) 
+?>
+
+</body>
+</html>
