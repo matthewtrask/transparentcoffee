7b2defa27ec9a8b2f613981ff0c8b6a0c9275b7f
matt trask <mjftrask@gmail.com>
2015-05-17 17:00:10 -0400

    added gulp task for watching sass files.

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
+gulp.task('styles', function(){
+	gulp.src('app/templates/default/scss/main.scss')
+		.pipe(sass({
+			errLogToConsole: true
+		}))
+		.pipe(gulp.dest('./css/'))
+
+});
+
+//watch task
 gulp.task('default', function(){
-	//add gulp tasks here	
+	gulp.watch('app/templates/default/scss/main.scss', ['styles']);
 });
