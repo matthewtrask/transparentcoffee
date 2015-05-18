a281c4d2beac5473ae3c8e25b783dfa371dfe665
matt trask <mjftrask@gmail.com>
2015-05-17 22:04:12 -0400

    set site title in config

--- public/app/core/config.php
+++ public/app/core/config.php
@@ -38,7 +38,7 @@ class Config {
 		define('SESSION_PREFIX', 'ttc');
 
 		//optionall create a constant for the name of the site
-		define('SITETITLE', '');
+		define('SITETITLE', 'Transparent Trade Coffee');
 
 		//turn on custom error handling
 		set_exception_handler('core\logger::exception_handler');
