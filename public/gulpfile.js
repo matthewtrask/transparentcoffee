5681d0f01fe012c144a2c6dd69611fef724612b0
matt trask <mjftrask@gmail.com>
2015-05-18 22:29:05 -0400

    updated node modules for gulp-minify-css
    not working just yet

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
 
+//Sass Task
 gulp.task('styles', function(){
 	gulp.src('app/templates/default/scss/*.scss')
 		.pipe(sass({
@@ -14,3 +16,10 @@ gulp.task('styles', function(){
 gulp.task('default', function(){
 	gulp.watch('app/templates/default/scss/*.scss', ['styles']);
 });
+
+//Minify Task
+gulp.task('minify-css', function(){
+	return gulp.src('styles/*.css')
+		.pipe(minifyCss({compatibilty: 'ie8'}))
+		.pipe(gulp.dest('dist'));
+})
