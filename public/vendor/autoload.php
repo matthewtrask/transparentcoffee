ff4f283734a296aa9b53c9d8543a107fb5c04cc1
matt trask <mjftrask@gmail.com>
2015-05-18 21:33:15 -0400

    updated composer file. nothing major.

--- public/vendor/autoload.php
+++ public/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit53f767233896db25b77b8de3c6d5d3a7::getLoader();
+return ComposerAutoloaderInit8d502db5871027eff0630bcfad281537::getLoader();
