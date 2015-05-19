d2db235a668aee5140d601fae3feca562bde707f
matt trask <mjftrask@gmail.com>
2015-05-18 22:28:06 -0400

    building footer for TTC

--- public/app/templates/default/footer.php
+++ public/app/templates/default/footer.php
@@ -1,7 +1,25 @@
 
 </div>
 
-<footer>
+<footer class="ttcFooter">
+    <div class="row">
+        <div class="small-3 medium-3 large-3 columns">
+            <div class="left">
+                <ul>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">TT COFFEES</a></li>
+                    <li>REGISTER</li>
+                    <li>INSIGHTS</li>
+                    <li>SCRPI</li>
+                    <li>CONTACT</li>
+                </ul>
+            </div>
+        </div>
+        <div class="small-3 medium-3 large-3 columns">
+        </div>
+        <div class="small-3 medium-3 large-3 columns">
+        </div>
+    </div>
 </footer>
 <script src="/bower_components/foundation/js/foundation.min.js"></script>
 <script>
