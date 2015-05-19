f1da1c8645bc8c19eacd6105cbda05417c6210ff
jrec15 <jrec15@uga.edu>
2015-05-18 21:36:18 -0400

    javascript loading properly

--- public/app/templates/default/header.php
+++ public/app/templates/default/header.php
@@ -6,6 +6,7 @@
 	<meta charset="utf-8">
 	<title><?php echo $data['title'].' | '.SITETITLE; //SITETITLE defined in app/core/config.php ?></title>
 	<link rel="stylesheet" type="text/css" href="/bower_components/foundation/css/foundation.css">
+    <script src="/bower_components/modernizr/modernizr.js"></script>
 
 	<!-- CSS -->
 	<?php
@@ -18,39 +19,41 @@
 	<!-- JS -->
 	<?php 
 		helpers\assets::js(array(
-			helpers\url::template_path() . 'jquery.js',
+			helpers\url::template_path() . 'js/jquery.js',
 		))
 	?>
-
 </head>
 <body>
-<nav class="top-bar" data-topbar role="navigation" id="nav">
-    <ul class="title-area">
-        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-    </ul>
-    <section class="top-bar-section">
-        <ul class="center">
-            <li class="has-dropdown">
-                <a href="#">ABOUT</a>
-                <ul class="dropdown">
-                    <li><a href="#">Transparency</a></li>
-                    <li><a href="#">SE@G</a></li>
-                </ul>
-            </li>
-            <li><a href="#">TT COFFEES</a></li>
-            <li><a href="#">REGISTER</a></li>
-            <!-- icon -->
-            <li><a href="#">INSIGHTS</a></li>
-            <li class="has-dropdown">
-                <a href="#">SCRPI</a>
-                <ul class="dropdown">
-                    <li><a href="#">SCRPI</a></li>
-                    <li><a href="#">Roasters</a></li>
-                </ul>
-            </li>
-            <li><a href="#">Contact</a></li>
+<div class="contain-to-grid fixed">
+    <nav class="top-bar" data-topbar role="navigation" id="nav">
+        <ul class="title-area">
+            <li class="name"><!-- Leave this empty --></li>
+            <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
         </ul>
-    </section>
-</nav>
+        <section class="top-bar-section">
+            <ul class="center">
+                <li class="has-dropdown">
+                    <a href="#">ABOUT</a>
+                    <ul class="dropdown">
+                        <li><a href="#">Transparency</a></li>
+                        <li><a href="#">SE@G</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">TT COFFEES</a></li>
+                <li><a href="#">REGISTER</a></li>
+                <!-- icon -->
+                <li><a href="#">INSIGHTS</a></li>
+                <li class="has-dropdown">
+                    <a href="#">SCRPI</a>
+                    <ul class="dropdown">
+                        <li><a href="#">SCRPI</a></li>
+                        <li><a href="#">Roasters</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </section>
+    </nav>
+</div>
 
 <div class="container">
