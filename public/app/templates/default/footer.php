d821bc1cf9f623c9ee0ac7fb2e1b8ee39ce7d7b9
jrec15 <jrec15@uga.edu>
2015-05-19 11:31:26 -0400

    fixed mobile nav

--- public/app/templates/default/footer.php
+++ public/app/templates/default/footer.php
@@ -4,7 +4,7 @@
 <footer>
 </footer>
 <script src="/bower_components/foundation/js/foundation.min.js"></script>
-<script src="/bower_components/foundation/js/foundation/foundation.topbar.js"></script>
+<script src="/bower_components/fastclick/lib/fastclick.js"></script>
 <script>
     $(document).foundation();
 </script>
--- public/app/templates/default/header.php
+++ public/app/templates/default/header.php
@@ -25,39 +25,35 @@
 </head>
 <body>
 <div class="fixed">
-    <div class="row">
-        <div class = "large-12 columns">
-            <nav class="top-bar" data-topbar role="navigation" id="nav">
-                <ul class="title-area">
-                    <li class="name"><!-- Leave this empty --></li>
-                    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-                </ul>
-                <section class="top-bar-section">
-                    <ul class="center">
-                        <li class="has-dropdown">
-                            <a href="#">ABOUT</a>
-                            <ul id="nav-dropdown" class="dropdown">
-                                <li><a href="#">Transparency</a></li>
-                                <li><a href="#">SE@G</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">TT COFFEES</a></li>
-                        <li><a href="#">REGISTER</a></li>
-                        <!-- icon -->
-                        <li><a href="#">INSIGHTS</a></li>
-                        <li class="has-dropdown">
-                            <a href="#">SCRPI</a>
-                            <ul id="nav-dropdown" class="dropdown">
-                                <li><a href="#">SCRPI</a></li>
-                                <li><a href="#">Roasters</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Contact</a></li>
+    <nav class="top-bar" data-topbar role="navigation" id="nav">
+        <ul class="title-area">
+            <li class="name"><!-- Leave this empty --></li>
+            <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+        </ul>
+        <section class="top-bar-section">
+            <ul>
+                <li class="has-dropdown">
+                    <a href="#">ABOUT</a>
+                    <ul id="nav-dropdown" class="dropdown">
+                        <li><a href="#">Transparency</a></li>
+                        <li><a href="#">SE@G</a></li>
                     </ul>
-                </section>
-            </nav>
-        </div>
-    </div>
+                </li>
+                <li><a href="#">TT COFFEES</a></li>
+                <li><a href="#">REGISTER</a></li>
+                <!-- icon -->
+                <li><a href="#">INSIGHTS</a></li>
+                <li class="has-dropdown">
+                    <a href="#">SCRPI</a>
+                    <ul id="nav-dropdown" class="dropdown">
+                        <li><a href="#">SCRPI</a></li>
+                        <li><a href="#">Roasters</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </section>
+    </nav>
 </div>
 
 <div class="container">
