324b2f6946716118746ce6ac554db524136ac149
matt trask <mjftrask@gmail.com>
2015-05-18 23:03:38 -0400

    base structure. all of it there minus title tag.

--- public/app/templates/default/footer.php
+++ public/app/templates/default/footer.php
@@ -8,16 +8,22 @@
                 <ul>
                     <li><a href="#">ABOUT</a></li>
                     <li><a href="#">TT COFFEES</a></li>
-                    <li>REGISTER</li>
-                    <li>INSIGHTS</li>
-                    <li>SCRPI</li>
-                    <li>CONTACT</li>
+                    <li><a href="#">REGISTER</li>
+                    <li><a href="#">INSIGHTS</a></li>
+                    <li><a href="#">SCRPI</a></li>
+                    <li><a href="#">CONTACT</a></li>
                 </ul>
             </div>
         </div>
         <div class="small-3 medium-3 large-3 columns">
         </div>
         <div class="small-3 medium-3 large-3 columns">
+            <div class="right" id="socialIcons">
+                <ul>
+                    <li><i class="fa fa-facebook fa-2x"></i></li>
+                    <li><i class="fa fa-twitter fa-2x"></i></li>
+                </ul>
+            </div>
         </div>
     </div>
 </footer>
