6870c92ea81caa69232f43f648f72edccd295bb2
matt trask <mjftrask@gmail.com>
2015-05-21 22:21:01 -0400

    Skeleton of the contact form for main Contact page

---
+++ public/app/templates/default/contact.php
@@ -0,0 +1,34 @@
+<div class="row">
+    <p class="text-center">We are always interested in questions, comments and ideas from others who are interested in promoting transparency within specialty coffee markets. If you are one of these people and have something to share, please contact us using the link below.</p>
+</div>
+
+<form action="" method="post"n id="contactForm">
+    <div class="row">
+        <div class="large-6">
+            <div class="row">
+                <div class="small-2 centered columns">
+                    <label for="Name" class="right">Name:</label>
+                </div>
+                <div class="small-10 centered columns">
+                    <input class="right" type="text" placeholder="Name" for="Name" id="Name">
+                </div>
+            </div>
+            <div class="row">
+                <div class="small-2 centered columns">
+                    <label for="Email" class="right">Email:</label>
+                </div>
+                <div class="small-10 centered columns">
+                    <input class="right" type="text" placeholder="Email" for="Email" id="Email">
+                </div>
+            </div>
+            <div class="row">
+                <div class="small-2 centered columns">
+                    <label for="Message" class="right">Message:</label>
+                </div>
+                <div class="small-10 centered columns">
+                    <textarea class="right" type="text" placeholder="Message" for="Message" id="Message"></textarea>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
