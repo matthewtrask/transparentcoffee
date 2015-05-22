e64f11a3bf68b8e215757744bf8820b66a303e18
jrec15 <jrec15@uga.edu>
2015-05-22 14:41:40 -0400

    added text to beans picture

--- public/app/templates/default/home.php
+++ public/app/templates/default/home.php
@@ -1,7 +1,10 @@
-<div>
-    <img id="swap" data-interchange="[<?php echo helpers\url::template_path() . 'img/beans-in-hand-fourth.jpg'?>, (small)],
+<div class="img-overlay">
+    <img data-interchange="[<?php echo helpers\url::template_path() . 'img/beans-in-hand-fourth.jpg'?>, (small)],
     [<?php echo helpers\url::template_path() . 'img/beans-in-hand-half.jpg'?>, (medium)],
     [<?php echo helpers\url::template_path() . 'img/beans%20in%20hand.jpg'?>, (large)]
     "/>
     <noscript><img src="<?php echo helpers\url::template_path() . 'img/beans-in-hand-half.jpg'?>"></noscript>
+    <div class="text-wrapper">
+        <h1 id="suit-text">Transparent Trade Coffee</h1>
+    </div>
 </div>
