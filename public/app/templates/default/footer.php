bcc0f0c8724484d574956b39baeb157ee9f06073
jrec15 <jrec15@uga.edu>
2015-05-22 11:17:33 -0400

    added first image, trying out interchange

--- public/app/templates/default/footer.php
+++ public/app/templates/default/footer.php
@@ -31,6 +31,9 @@
 <script src="/bower_components/foundation/js/foundation.min.js"></script>
 <script src="/bower_components/fastclick/lib/fastclick.js"></script>
 <script>
+    $("#swap").on("replace", function() {
+        $(document).foundation();
+    });
     $(document).foundation();
 </script>
 </body>
---
+++ public/app/templates/default/home.php
@@ -0,0 +1,7 @@
+<div>
+    <img id="swap" data-interchange="[<?php echo helpers\url::template_path() . 'img/beans-in-hand-fourth.jpg'?>, (small)],
+    [<?php echo helpers\url::template_path() . 'img/beans-in-hand-half.jpg'?>, (medium)],
+    [<?php echo helpers\url::template_path() . 'img/beans%20in%20hand.jpg'?>, (large)]
+    "/>
+    <noscript><img src="<?php echo helpers\url::template_path() . 'img/beans-in-hand-half.jpg'?>"></noscript>
+</div>
