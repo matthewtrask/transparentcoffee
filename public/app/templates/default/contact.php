3a656f23c004921266abba6ad91786ec525fe906
matt trask <mjftrask@gmail.com>
2015-05-24 11:21:14 -0400

    Contact page so far. Working on more now

--- public/app/templates/default/contact.php
+++ public/app/templates/default/contact.php
@@ -2,31 +2,36 @@
     <p class="text-center">We are always interested in questions, comments and ideas from others who are interested in promoting transparency within specialty coffee markets. If you are one of these people and have something to share, please contact us using the link below.</p>
 </div>
 
-<form action="" method="post"n id="contactForm">
+<form action="" method="post" id="contactForm" data-abide>
     <div class="row">
         <div class="large-6">
             <div class="row">
                 <div class="small-2 centered columns">
-                    <label for="Name" class="right">Name:</label>
+                    <label for="Name" class="right inline">Name:</label>
                 </div>
                 <div class="small-10 centered columns">
-                    <input class="right" type="text" placeholder="Name" for="Name" id="Name">
+                    <input class="formInput right" type="text" placeholder="Name" for="Name" id="Name" required>
                 </div>
             </div>
             <div class="row">
                 <div class="small-2 centered columns">
-                    <label for="Email" class="right">Email:</label>
+                    <label for="Email" class="right inline">Email:</label>
                 </div>
                 <div class="small-10 centered columns">
-                    <input class="right" type="text" placeholder="Email" for="Email" id="Email">
+                    <input class="formInput right" type="email" placeholder="Email" for="Email" id="Email" required>
                 </div>
             </div>
             <div class="row">
                 <div class="small-2 centered columns">
-                    <label for="Message" class="right">Message:</label>
+                    <label for="Message" class="right inline">Message:</label>
                 </div>
                 <div class="small-10 centered columns">
-                    <textarea class="right" type="text" placeholder="Message" for="Message" id="Message"></textarea>
+                    <textarea class="formInput right" type="text" placeholder="Message" for="Message" id="Message" required></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="small-5 centered columns">
+                    <input type="submit" value="Submit" class="button expand left">
                 </div>
             </div>
         </div>
