8c9279385996380d84fa7913728e5b06d81ea487
jrec15 <jrec15@uga.edu>
2015-05-30 10:54:22 -0400

    adding captions to slick carousel

--- public/app/templates/default/home.php
+++ public/app/templates/default/home.php
@@ -19,6 +19,28 @@
     </div>
 </div>
 <div class="slick single-item autoplay">
-    <div><img src="<?php echo helpers\url::template_path() . 'img/CounterCulture1.jpg'?>"/></div>
-    <div><img src="<?php echo helpers\url::template_path() . 'img/red-beans.JPG'?>"/></div>
+    <div class="slide slide--has-caption slick-slide">
+        <div class="img-overlay-2">
+            <img src="<?php echo helpers\url::template_path() . 'img/CounterCulture1.jpg'?>"/>
+        </div>
+        <div class="slide-title">Featured Roasters</div>
+        <div class="slide-caption">
+            <div class="sub-title">Counter Culture</div>
+            At Counter Culture Coffee, we approach our work with a relentless pursuit of coffee perfection,
+            a dedication to real sustainability, and a commitment to cutting-edge coffee education throughout the coffee chain.
+            <br><a href="#" class="button white-button">READ MORE</a>
+        </div>
+    </div>
+    <div class="slide slide--has-caption slick-slide">
+        <div class="img-overlay-2">
+            <img src="<?php echo helpers\url::template_path() . 'img/red-beans.JPG'?>"/>
+        </div>
+        <div class="slide-title">Featured Roasters</div>
+        <div class="slide-caption">
+            <div class="sub-title">Farmers to 40</div>
+            Farmers to 40 is a social enterprise providing excellent coffee and returning 40%
+            of the retail bag price directly to our partner farmers in Nicaragua.
+            <br><a href="#" class="button white-button">READ MORE</a>
+        </div>
+    </div>
 </div>
--- public/app/templates/default/js/slick.js
+++ public/app/templates/default/js/slick.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     $('.slick').slick({
+        infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 5000,
-        dots: true
+        dots: true,
     });
 });
