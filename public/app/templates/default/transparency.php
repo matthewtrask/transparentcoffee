130ec1c5bd203331c14bbe4b834a6e305e612929
matt trask <mjftrask@gmail.com>
2015-06-09 17:10:13 -0400

    the transparency page

---
+++ public/app/templates/default/transparency.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthewtrask
+ * Date: 6/9/15
+ * Time: 2:45 PM
+ */
+?>
+<div class="img-overlay">
+    <img data-interchange="[<?php echo helpers\url::template_path() . 'img/transparency-quarter.jpg'?>, (small)],
+    [<?php echo helpers\url::template_path() . 'img/transparency-half.jpg'?>, (medium)],
+    [<?php echo helpers\url::template_path() . 'img/transparency.jpg'?>, (large)]
+    "/>
+    <noscript><img src="<?php echo helpers\url::template_path() . 'img/transparency-half.png'?>"></noscript>
+    <div class="text-wrapper">
+        <h1 id="coffee-text">Transparency</h1>
+    </div>
+</div>
+<div style="height:20px">
+
+</div>
+
+<div class="row">
+    <div class="small-12 medium-12 large-12">
+        <p>Coffee growers are primarily responsible for the excellent coffees that we drink. This is why, more and more, specialty coffee markets are recognizing and celebrating the many contributions of these growers.</p>
+        <p>However, the current structure of global coffee markets is such that these growers have a hard time being adequately compensated for the work that they do on their farms and in their communities.</p>
+        <p>It is time to clearly and succinctly inform coffee consumers about the economic treatment of coffee growers.</p>
+        <p>To accomplish this goal, <b>Transparent Trade Coffee</b> provides:
+            <ul class="reasons">
+                <li> - A forum for direct trade roasters that are genuinely committed to transparency in their dealings
+                    with coffee growers; and</li>
+                <li> - A simple communication vehicle so that consumers can – at the time of purchase – know how
+                    much of their coffee dollars are going back to growers.</li>
+            </ul>
+        </p>
+        <p><b>Clearly, there is more to specialty coffee purchases and communications than the green prices that are paid to growers. We are keenly interested in the many nuances of coffee quality, and are concerned about the various social and environmental contributions of coffee growers. However, an effective launching point into all of these conversations is a clear articulation of the effective share of retail prices that is going to growers.</b></p>
+    </div>
+</div>
+<div style="height: 50px">
+    
+</div>
