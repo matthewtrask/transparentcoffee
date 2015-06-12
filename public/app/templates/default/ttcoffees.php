20d3365ce9f7bc036eb5185cd2de11fedb4e775f
matt trask <mjftrask@gmail.com>
2015-06-11 23:32:23 -0400

    tic template. not finished yet.

---
+++ public/app/templates/default/ttcoffees.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthewtrask
+ * Date: 6/11/15
+ * Time: 8:19 PM
+ */
+?>
+<div class="img-overlay">
+    <img src="<?php echo helpers\url::template_path() . 'img/Baskets_small.jpg'?>">
+    <div class="text-wrapper">
+        <h1 class="ttcoffee-header image-text">Transparent Coffees</h1>
+    </div>
+</div>
+<div class="row featuredCoffees" style="margin-top: 20px">
+  <div class="small-2 medium-2 large-2 columns">
+  </div>
+  <div class="small-8 medium-8 large-8 small-centered medium-centered large-centered columns">
+    <h2 class="text-center">Featured Roaster</h2><hr />
+    <div class="slick single-item autoplay" id="">
+      <div class="featuredRoasters slick single-item autoplay ">
+          <div class="slide slick-slide featuredRoaster"><img src="<?php echo \helpers\url::template_path() . "img/CounterCultureLogo.jpg" ?>"></div>
+          <div class="slide slick-slide featuredRoaster"><img src="<?php echo \helpers\url::template_path() . "img/BirdRock.png"?>"></div>
+      </div>
+    </div>
+  </div>
+  <div class="small-2 medium-2 large-2 columns">
+  </div>
+</div>
+
+<div class="row">
