ea481684253d5ac66be8a233458dba3a145d612c
matt trask <mjftrask@gmail.com>
2015-06-11 23:31:24 -0400

    changed slider speed to make it a little faster

--- public/app/templates/default/js/slick.js
+++ public/app/templates/default/js/slick.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 3000,
         dots: true,
     });
 });
