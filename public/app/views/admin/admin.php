315cf99213173f27178577bce0092853e9906abe
matt trask <mjftrask@gmail.com>
2015-07-09 18:21:43 -0400

    beginning of the admin approval

--- public/app/views/admin/admin.php
+++ public/app/views/admin/admin.php
@@ -18,16 +18,44 @@
 </div>  
 
 <div class="row approval">
-	<div class="small-12 medium-12 large-12 columns">
+	<div class="small-8 medium-8 large-8 columns">
 		<h1>Registration Approval</h1>
 		<table>
 			<tr>
 				<th>Name</th>
-				<th>Phone</th>
+				<th>Email</th>
+				<th>Roaster Name</th>
+				<th>Roaster Description</th>
+				<th>Roaster Logo</th>
+				<th>Coffee Name</th>
+				<th>Coffee Descrpition</th>
+				<th>Retail Price</th>
+				<th>Currency</th>
+				<th>Bag Size(in ounces)</th>
+				<th>Green PPP</th>
+				<th>Website</th>
+				<th>Farm Name</th>
+				<th>Farm Country</th>
+				<th>Farm Region</th>
+				<th>GPPP Proof</th>
 			</tr>
 			<tr>
-				<td>John Smith</td>
-				<td>111-111-1111</td>
+				<td>Test</td>
+				<td>test@test.com</td>
+				<td>Counter Culture</td>
+				<td>Roaster Descripdion</td>
+				<td>Roaster Logo</td>
+				<td>Coffee Name</td>
+				<td>Coffee Descrpition</td>
+				<td>Retail Price</td>
+				<td>Currency</td>
+				<td>Bag Size(in ounces)</td>
+				<td>Green PPP</td>
+				<td>Website</td>
+				<td>Farm Name</td>
+				<td>Farm Country</td>
+				<td>Farm Region</td>
+				<td>GPPP Proof</td>
 			</tr>
 		</table>
 	</div>
