8a224c72999901b839fbad7c6c27089c6b17a2c1
jrec15 <jrec15@uga.edu>
2015-07-11 20:16:14 -0400

    minor change to thankyou

--- public/app/templates/default/thankyou.php
+++ public/app/templates/default/thankyou.php
@@ -7,5 +7,7 @@
  */
 ?>
 <div class="row" style="margin-top: 50px; margin-bottom: 50px;">
-    <h2 class="page-header">Thank you for your submission! We will contact you when you are approved or if we need further information.</h2>
+    <div class="small-offset-1 small-10 columns">
+        <h2 class="page-header">Thank you for your submission! We will contact you when you are approved or if we need further information.</h2>
+    </div>
 </div>
