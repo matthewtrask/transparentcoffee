e783099bbc7382563b42d80cf6603ea3221687f8
matt trask <mjftrask@gmail.com>
2015-07-12 10:33:52 -0400

    added the twitter share, facebook is still not working. altered meta tags as well for twitter

--- public/app/views/blog/post.php
+++ public/app/views/blog/post.php
@@ -19,9 +19,10 @@
 					echo "<p>Posted on ".date('jS M Y H:i:s', strtotime($row->postDate))." in <a href='".DIR."category/$row->catSlug'>$row->catTitle</a></p>";
 
 					//echo "<p><img src='".DIR."$row->postImg' class='img-responsive'></p>";
-
-
+					echo "<div class='fb-share-button' data-href='thttp://transparenttradecoffee.com/insights' data-layout='button'></div>";
+					echo "<a class='twitter-share-button' id='twitter-wjs' href='https://twitter.com/intent/tweet?text='A%20new%20post%20from%20TTC'  data-size='large'> Tweet</a>";
 					echo "<div class='content'>".stripslashes($row->postCont)."</div>";
+
 				echo "</div>\n";
 			}
 		}
