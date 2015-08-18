26f147672c3d2a52e5ceb98f2db3c3bf250e82dc
jrec15 <jrec15@uga.edu>
2015-08-18 15:11:09 -0400

    small tweak on 404

--- public/app/views/error/404.php
+++ public/app/views/error/404.php
@@ -2,12 +2,13 @@
     <div class="small-12 medium-12 large-12 centered columns">
         <h1 class="text-center">Oops!</h1>
         <p class="text-center">You seemed to have run out of coffee!</p>
-        <div class='small-8 medium-4 small-offset-3 medium-offset-5 columns'>
+        <div class="small-10 medium-6 small-offset-1 medium-offset-3 columns">
+        <div class='small-6 medium-6 large-4 small-offset-3 medium-offset-3 large-offset-4 columns'>
             <img style="margin-left: auto; margin-right: auto; float: none !important" class="img-responsive center-block" src="<?php echo helpers\url::template_path() . 'img/heart-coffee.jpg'?>"/>
-        </div><br><br><br><br><br><br><br>
+        </div></div><br><br><br><br><br><br><br>
         <p class="text-center">Click the button below to go back and get more coffee</p>
-        <div class='small-8 medium-4 small-offset-2 medium-offset-5 columns'>
-            <button class="flat-white-btn button"><a href="/">Home</a></button>
+        <div class='small-6 medium-2 small-offset-3 medium-offset-5 columns'>
+            <a class="flat-white-btn button expand" href="/">Home</a>
         </div>
     </div>
 </div>
